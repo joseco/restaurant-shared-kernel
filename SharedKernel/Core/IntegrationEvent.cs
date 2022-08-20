1dcd768d5f76d0d30a15dd7cc2714193472e1ea9
Jose Carlos Gutierrez <joseco48@gmail.com>
2022-08-20 10:46:06 -0400

    Se agregaron eventos de integracion

---
+++ SharedKernel/Core/IntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.Core
+{
+    public abstract record IntegrationEvent 
+    {
+        public DateTime OccuredOn { get;  }
+        public Guid EventId { get; set; }
+        public IntegrationEvent()
+        {
+            EventId = Guid.NewGuid();
+            OccuredOn = DateTime.UtcNow;
+        }
+    }
+}
---
+++ SharedKernel/IntegrationEvents/ArticuloCreado.cs
@@ -0,0 +1,17 @@
+﻿using SharedKernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.IntegrationEvents
+{
+    public record ArticuloCreado : IntegrationEvent
+    {
+        public Guid ArticuloId { get; set; }
+        public string Nombre { get; set; }
+
+        public decimal PrecioVenta { get; set; }
+    }
+}
