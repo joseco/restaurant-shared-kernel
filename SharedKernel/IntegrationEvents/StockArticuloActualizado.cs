4580104e48493e402bcd5bfd12bef12026e2c672
Jose Carlos Gutierrez <joseco48@gmail.com>
2022-08-27 10:48:36 -0400

    Se agrego el Evento de Integracion StockArticuloActualizado

---
+++ SharedKernel/IntegrationEvents/StockArticuloActualizado.cs
@@ -0,0 +1,15 @@
+﻿using SharedKernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.IntegrationEvents
+{
+    public record  StockArticuloActualizado : IntegrationEvent
+    {
+        public Guid ArticuloId { get; set; }
+        public decimal NuevoStock { get; set; }
+    }
+}
